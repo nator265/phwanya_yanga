f5f1552709afabd56d9b6848415aa26d58c6280a
Nator265 <kelvinzimba2322000@gmail.com>
2024-09-21 01:05:54 +0200

    theme toggle

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,44 +1,65 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Phwanya Yanga",
-  description: "An online restaurant platform that is meant to allow customers to make orders online from anywhere",
+	title: "Phwanya Yanga",
+	description:
+		"An online restaurant platform that is meant to allow customers to make orders online from anywhere",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-        <meta name="msapplication-TileColor" content="#00aba9"/>
-        <meta name="theme-color" content="#ffffff"/>
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<head>
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/site.webmanifest' />
+				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+				<meta name='msapplication-TileColor' content='#00aba9' />
+				<meta name='theme-color' content='#ffffff' />
+			</head>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { ModeToggle } from "../components/ui/ModeToggle"
 
 const page = () => {
-  return (
-	<div>page</div>
-  )
-}
+	return (
+		<div>
+			<div>
+				<div>
+					page
+				</div>
+				<div>
+					<ModeToggle/>
+				</div>
+			</div>
+		</div >
+	)
+		
+};
 
-export default page
+export default page;
